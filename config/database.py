fe2d06919e93cad7befc7de449d0b36776a62a86
Andres Acosta <andresdavidacostasarmiento@gmail.com>
2023-03-15 19:45:34 -0500

    configuracion BD y estructura

---
+++ config/database.py
@@ -0,0 +1,21 @@
+# to use this need execute the command pip install mysql-connector-python
+import mysql.connector
+
+
+class Conexion:
+    
+    _database = 'sistema_reglas'
+    _user = 'root'
+    _password = ''
+    _host = 'localhost'
+
+    _conecction = mysql.connector.connect(
+        host=_host,
+        user=_user,
+        passwd=_password,
+        db=_database
+    )
+
+    def get_connection(self):
+        return self._conecction
+
