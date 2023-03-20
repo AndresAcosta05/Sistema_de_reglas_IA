454e0f0909c8a2cd033c2177dfa51f8041c57b66
carls1720 <quintero.carlos.rocha@gmail.com>
2023-03-19 23:11:22 -0500

    avance

--- SBR_sistema.py
+++ SBR_sistema.py
@@ -1,67 +1,66 @@
 from SBR_reglas import *
 
 class sistema(KnowledgeEngine):
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s1(self):
+    @Rule(AND(NOT(reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n1(self):
         print("Salud estable")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s2(self):
-        print("Tos cronica")
-        
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s3(self):
-        print("Disnea")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s4(self):
-        print("Posible asma")
-        
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s5(self):
-        print("Estres emocional")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s6(self):
-        print("Resfriado")
-        
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s7(self):
-        print("Posible neumonia")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
-    def s8(self):
-        print("Resfriado o posible neumonia")   
-        
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s9(self):
-        print("Hemoptisis")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s10(self):
-        print("Posible bronquitis")
-        
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s11(self):
-        print("Posible bronquiectasia")    
-    
-    @Rule(AND((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s12(self):
-        print("Posible vasculitis")
-        
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s13(self):
-        print("Posible tuberculosis")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s14(self):
-        print("Neumonia grave")               
 
-    @Rule(AND(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s15(self):
-        print("Posible Cancér de pulmón")
-        
-    @Rule(AND((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),((reglas(tos_sangre="si"))))
-    def s16(self):
-        print("Cancér de pulmón")
-        
+    @Rule(AND((reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n2(self):
+        print("Alergía") 
+
+    @Rule(AND(NOT(reglas(estornudos="si"))),((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n3(self):
+        print("Gripe común  ")
+
+    @Rule(AND((reglas(estornudos="si"))),((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n4(self):
+        print("Tos crónica")       
+
+    @Rule(AND(NOT(reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n5(self):
+        print("Disnea")
+
+    @Rule(AND((reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n6(self):
+        print("Pleusería") 
+
+    @Rule(AND(NOT(reglas(estornudos="si"))),((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n7(self):
+        print("Asma  ")
+
+    @Rule(AND((reglas(estornudos="si"))),((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n8(self):
+        print("Bronquitis aguda")  
+
+    @Rule(AND(NOT(reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n9(self):
+        print("Estrés emocional")
+
+    @Rule(AND((reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n10(self):
+        print("Rinitis alergica") 
+
+    @Rule(AND((reglas(estornudos="si"))),((reglas(tos_persistente="si"))),(NOT(reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n11(self):
+        print("Influenza ")
+
+    @Rule(AND(NOT(reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n12(self):
+        print("Neumonía")       
+
+    @Rule(AND((reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),((reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n13(self):
+        print("Virus respiratorio sinicial")
+
+    @Rule(AND((reglas(estornudos="si"))),(NOT(reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n14(self):
+        print("Pleusería") 
+
+    @Rule(AND(NOT(reglas(estornudos="si"))),((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n15(self):
+        print("Asma  ")
+
+    @Rule(AND((reglas(estornudos="si"))),((reglas(tos_persistente="si"))),((reglas(dolor_pecho="si"))),(NOT(reglas(df_respirar="si"))),(NOT(reglas(tos_sangre="si"))))
+    def n16(self):
+        print("Bronquitis aguda") 
